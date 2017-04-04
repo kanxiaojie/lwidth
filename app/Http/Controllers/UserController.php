59b901b4b26a74a2728d7e3c2df1c87c1da68e54
kanxiaojie <kanhappyday2008@163.com>
2017-04-04 20:46:42 +0800

    update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Country;
+use App\Gender;
+use App\Profile;
 use App\Repositories\BaseRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
@@ -51,7 +54,8 @@ class UserController extends Controller
         $inputs = $request->all();
 
         $openid = Crypt::decrypt($inputs['wesecret']);
-
+        $userInfo = [];
+        $data = [];
         $user = $this->userRepository->getUserByOpenId($openid);
         if($user)
         {
@@ -66,6 +70,164 @@ class UserController extends Controller
                 {
                     $updateUser = $this->userRepository->updateUser($inputs,$user,$res['picturePath']);
 
+                    $userInfo['id'] = $updateUser->id ;
+                    $userInfo['nickName'] = $updateUser->nickname ;
+                    $userInfo['avatarUrl'] = $updateUser->avatarUrl ;
+                    $userInfo['gender'] = Gender::where('id',$userInfo->gender)->first()->name ;
+                    $userInfo['province'] = "福建";
+                    $userInfo['city'] = "厦门";
+                    if($updateUser->country_id)
+                    {
+                        $userInfo['country'] = Country::where('id',$updateUser->country_id)->first()->name;
+                    }
+                    else
+                    {
+                        $userInfo['country'] = "中国";
+                    }
+
+                    if($updateUser->realname)
+                    {
+                        $userInfo['realname'] = $updateUser->realname;
+                    }
+                    else
+                    {
+                        $userInfo['realname'] = '';
+                    }
+
+                    $userInfo['college'] = '';
+                    if($updateUser->major)
+                    {
+                        $userInfo['major'] = $updateUser->major;
+                    }
+                    else
+                    {
+                        $userInfo['major'] = '';
+                    }
+
+                    if($updateUser->grade)
+                    {
+                        $userInfo['grade'] = $updateUser->grade;
+                    }else
+                    {
+                        $userInfo['grade'] = '';
+                    }
+
+                    if($updateUser->wechat)
+                    {
+                        $userInfo['wechat'] = $updateUser->wechat;
+                    }else
+                    {
+                        $userInfo['wechat'] = '';
+                    }
+
+                    if($updateUser->QQ)
+                    {
+                        $userInfo['qq'] = $updateUser->QQ;
+                    }else
+                    {
+                        $userInfo['qq'] = '';
+                    }
+
+                    if($updateUser->weibo)
+                    {
+                        $userInfo['weibo'] = $updateUser->weibo;
+                    }else
+                    {
+                        $userInfo['weibo'] = '';
+                    }
+
+                    if($updateUser->phone)
+                    {
+                        $userInfo['mobilePhone'] = $updateUser->phone;
+                    }else
+                    {
+                        $userInfo['mobilePhone'] = '';
+                    }
+
+                    $profile = Profile::where('user_id',$updateUser->id)->first();
+
+                    if($profile->birthday)
+                    {
+                        $userInfo['birthday'] = $profile->birthday;
+                    }
+                    else
+                    {
+                        $userInfo['birthday'] = '';
+                    }
+
+                    if($profile->height)
+                    {
+                        $userInfo['height'] = $profile->height;
+                    }
+                    else
+                    {
+                        $userInfo['height'] = '';
+                    }
+
+                    if($profile->weight)
+                    {
+                        $userInfo['weight'] = $profile->weight;
+                    }
+                    else
+                    {
+                        $userInfo['weight'] = '';
+                    }
+
+                    if($profile->hometown)
+                    {
+                        $userInfo['hometown'] = $profile->hometown;
+                    }
+                    else
+                    {
+                        $userInfo['hometown'] = '';
+                    }
+
+                    if($profile->signature)
+                    {
+                        $userInfo['signature'] = $profile->signature;
+                    }
+                    else
+                    {
+                        $userInfo['signature'] = '';
+                    }
+
+                    if($profile->character)
+                    {
+                        $userInfo['character'] = $profile->character;
+                    }
+                    else
+                    {
+                        $userInfo['character'] = '';
+                    }
+
+                    if($profile->hobby)
+                    {
+                        $userInfo['hobby'] = $profile->hobby;
+                    }
+                    else
+                    {
+                        $userInfo['hobby'] = '';
+                    }
+
+                    if($profile->love_history)
+                    {
+                        $userInfo['love_history'] = $profile->love_history;
+                    }
+                    else
+                    {
+                        $userInfo['love_history'] = '';
+                    }
+
+                    if($profile->love_selecting)
+                    {
+                        $userInfo['love_selecting'] = $profile->love_selecting;
+                    }
+                    else
+                    {
+                        $userInfo['love_selecting'] = '';
+                    }
+
+                    return response()->json(['status' => 200,'data' => $userInfo]);
 
                 }
             }
