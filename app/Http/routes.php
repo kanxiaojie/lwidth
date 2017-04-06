cd565027203caca8061082e8955fd8c8af1d3d67
kanxiaojie <kanhappyday2008@163.com>
2017-04-06 15:48:55 +0800

    routes.php fixed

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,10 +44,13 @@ $app->get('/api/loves/{id}/comments','PostController@postAndSelfComments');
 
 //给某条表白评论
 $app->post('/api/loves/{id}/comments','CommentController@publishComments');
+
 //给某条表白点赞/或取消点赞
 $app->post('/api/loves/{id}/praises','PraiseController@praiseToPost');
+
 //给某条评论再评论
 $app->post('/api/comments/{id}/comments','CommentController@commentToComment');
+
 //获取某条评论的所有再评论
 $app->get('/api/comments/{id}/comments','CommentController@getCommentToComments');
 
