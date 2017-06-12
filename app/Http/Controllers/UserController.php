3aad2cacc39d1580b22bc915174af4486b030602
曾昌林 <changlin.zeng@shatacloud.com>
2017-06-12 19:52:08 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -668,7 +668,13 @@ class UserController extends Controller
                         // $data['college_name'] = College::find($user->college_id)->name;
                         // $data['college_name'] = College::find((int)$user->college_id)->name;
                         // dd($user->id);
-                        $data['college_name'] = College::where('id',(int)($user->college_id))->first()->name;             
+                        if(empty($user->college_id)) {
+                            $data['college_name'] = '';
+                        } else {
+                            $data['college_name'] = College::find($user->college_id)->name;
+                            
+                            // $data['college_name'] = College::where('id',(int)($user->college_id))->first()->name;             
+                        }
 
                         if(empty($user->pictures))
                         { 
