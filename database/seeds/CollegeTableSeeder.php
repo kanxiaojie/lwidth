c6190207ab7e47deb9302d21ee532a86cf7bcbe6
曾昌林 <changlin.zeng@shatacloud.com>
2017-06-23 16:28:58 +0800

    changed db:seed and default-collegeId

--- database/seeds/CollegeTableSeeder.php
+++ database/seeds/CollegeTableSeeder.php
@@ -14,61 +14,7 @@ class CollegeTableSeeder extends Seeder
     public function run()
     {
         College::create([
-            'name'=>'福州大学'
-        ]);
-        College::create([
-            'name'=>'福建师范大学'
-        ]);
-        College::create([
-            'name'=>'福建师大协和学院'
-        ]);
-        College::create([
-            'name'=>'福建医科大学'
-        ]);
-        College::create([
-            'name'=>'福建中医药大学'
-        ]);
-        College::create([
-            'name'=>'福建农林大学'
-        ]);
-        College::create([
-            'name'=>'福建工程学院'
-        ]);
-        College::create([
-            'name'=>'闽江学院'
-        ]);
-        College::create([
-            'name'=>'江夏学院'
-        ]);
-        College::create([
-            'name'=>'福州教育学院'
-        ]);
-        College::create([
-            'name'=>'华南女子学院'
-        ]);
-        College::create([
-            'name'=>'福州职业技术学院'
-        ]);
-        College::create([
-            'name'=>'福州大学至诚学院'
-        ]);
-        College::create([
-            'name'=>'福州大学阳光学院'
-        ]);
-        College::create([
-            'name'=>'福建农林大学金山学院'
-        ]);
-        College::create([
-            'name'=>'福建农林大学东方学院'
-        ]);
-        College::create([
-            'name'=>'福建警察学院'
-        ]);
-        College::create([
-            'name'=>'福州外语外贸学院'
-        ]);
-        College::create([
-            'name'=>'平潭海洋大学'
+            'name'=>'天津机电职业技术学院'
         ]);
     }
 }
