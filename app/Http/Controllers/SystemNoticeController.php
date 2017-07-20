378b140d6b309d9deb8e9acf06a773141dd0bdb9
曾昌林 <changlin.zeng@shatacloud.com>
2017-07-20 10:32:44 +0800

    update

--- app/Http/Controllers/SystemNoticeController.php
+++ app/Http/Controllers/SystemNoticeController.php
@@ -207,6 +207,6 @@ class SystemNoticeController extends Controller
 
 
     public function get_available(Request $request) {
-        return response()->json(['status' => 200,'data' => 0]);
+        return response()->json(['status' => 200,'data' => 1]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,13 +14,15 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        $this->call(LanguageTableSeeder::class);
-        $this->call(CountryTableSeeder::class);
-        $this->call(GenderTableSeeder::class);
-        $this->call(CollegeTableSeeder::class);
-        $this->call(GradeTableSeeder::class);
         $this->call(UserTableSeeder::class);
+        $this->call(GenderTableSeeder::class);
         $this->call(BadReportTypesTableSeeder::class);
+        $this->call(LanguageTableSeeder::class);
+        
+        // $this->call(CountryTableSeeder::class);
+        // $this->call(CollegeTableSeeder::class);
+        // $this->call(GradeTableSeeder::class);
+        
 
         Model::reguard();
     }
