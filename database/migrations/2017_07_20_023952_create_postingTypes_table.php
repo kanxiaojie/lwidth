f4523949319ab951ef4c8acccd1ed861433a6b8a
曾昌林 <changlin.zeng@shatacloud.com>
2017-07-21 11:35:42 +0800

    update

--- database/migrations/2017_07_20_023952_create_postingTypes_table.php
+++ database/migrations/2017_07_20_023952_create_postingTypes_table.php
@@ -14,6 +14,7 @@ class CreatePostingTypesTable extends Migration
     {
         Schema::create('postingTypes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
