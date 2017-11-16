1ab6ff0e9846353caddac0aff0b974227b499d67
linchanglin <493043444@qq.com>
2017-11-16 17:31:54 +0800

    update

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -129,10 +129,11 @@ class WeixinController extends Controller
         //拼接成字符串,sha1加密 ，然后与signature进行校验
         $str = sha1( implode( $array ) );
         if( $str == $signature && $echostr ){
-            //第一次接入weixin api接口的时候
-            return  $echostr;
+            ob_clean();
+            echo $echostr;
+            exit;
         }
-        // return 'nothing';
+        echo 'nothing';
 
     }
 
