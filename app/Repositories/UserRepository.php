1184c130a77a670c9cf59672f6173ef45b5d53fd
linchanglin <493043444@qq.com>
2017-12-18 21:05:16 +0800

    update

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -395,7 +395,7 @@ class UserRepository
                             }
                         })
                         ->where(['available' => 1, 'pictureOnWall' => 1, 'role' => 1])
-                        ->where('college_id', '<>', null)
+                        ->where('college_id', '<>', '')
                         ->orderBy('praiseNums', 'desc')->orderBy('created_at', 'desc')->paginate(15);
 
         return $users;
