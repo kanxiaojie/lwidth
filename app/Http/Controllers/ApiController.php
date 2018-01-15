4dd1ebdb3d4b89f1df599837ba4ebe4c05ccf0ff
linchanglin <493043444@qq.com>
2018-01-15 21:40:24 +0800

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -399,7 +399,6 @@ class ApiController extends Controller
         
         $response = $this->http_post_data($url, $data);
 
-        var_dump("send_templateMessage response:  ".$response);
         return response()->json(['status' => 200,'response' => $response]);
     }
 
