a63136002f7ae1a9c98fd333f311b196aad6d283
linchanglin <493043444@qq.com>
2018-02-07 15:13:23 +0800

    fix

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -454,11 +454,11 @@ class PostRepository
 
         
 
-        if(isset($inputs['anonymous']) && !empty($inputs['anonymous']))
+        if(isset($inputs['anonymous']))
         {
             $post->anonymous = $inputs['anonymous'];
         }
-        if(isset($inputs['available']) && !empty($inputs['available']))
+        if(isset($inputs['available']))
         {
             $post->available = $inputs['available'];
         }
